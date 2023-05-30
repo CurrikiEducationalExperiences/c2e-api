375cd7212637fe6541a073e3f2bf017ce4bbcde2
Niket Doshi <niket.doshi@rediffmail.com>
2023-05-30 17:28:56 +0530

    Handled CORS

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-// Import Express.js
 import express from 'express'
 
-
-// This variable defines the port of your computer where the API will be available
 const PORT = 5001
 
-// This variable instantiate the Express.js library
 const app = express()
 
 
-// The code below starts the API with these parameters:
-// 1 - The PORT where your API will be available
-// 2 - The callback function (function to call) when your API is ready
+import cors from 'cors'
+
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+app.use(cors(corsOpts));
+
 app.listen(PORT, () =>
   console.log(`The Books API is running on: http://localhost:${PORT}.`)
 )
